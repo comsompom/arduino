bd989bfe018f7996581311dbfdb36fa9bcf36369
comsompom <gelfrad@yandex.com>
2024-07-04 11:36:34 +0300

    change flight settings

--- YMFC-AL/YMFC_AL_Flight_controller_M2/YMFC_AL_Flight_controller_M2.ino
+++ YMFC-AL/YMFC_AL_Flight_controller_M2/YMFC_AL_Flight_controller_M2.ino
@@ -21,20 +21,20 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //PID gain and limit settings
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-float pid_p_gain_roll = 1.3;               //Gain setting for the roll P-controller
-float pid_i_gain_roll = 0.04;              //Gain setting for the roll I-controller
-float pid_d_gain_roll = 18.0;              //Gain setting for the roll D-controller
-int pid_max_roll = 400;                    //Maximum output of the PID-controller (+/-)
+float pid_p_gain_roll = 1.5;               //Gain setting for the roll P-controller - def 1.3
+float pid_i_gain_roll = 0.06;              //Gain setting for the roll I-controller - def 0.04
+float pid_d_gain_roll = 15.0;              //Gain setting for the roll D-controller - def 18.0
+int pid_max_roll = 350;                    //Maximum output of the PID-controller (+/-) - def 400
 
 float pid_p_gain_pitch = pid_p_gain_roll;  //Gain setting for the pitch P-controller.
 float pid_i_gain_pitch = pid_i_gain_roll;  //Gain setting for the pitch I-controller.
 float pid_d_gain_pitch = pid_d_gain_roll;  //Gain setting for the pitch D-controller.
 int pid_max_pitch = pid_max_roll;          //Maximum output of the PID-controller (+/-)
 
-float pid_p_gain_yaw = 4.0;                //Gain setting for the pitch P-controller. //4.0
-float pid_i_gain_yaw = 0.02;               //Gain setting for the pitch I-controller. //0.02
-float pid_d_gain_yaw = 0.0;                //Gain setting for the pitch D-controller.
-int pid_max_yaw = 400;                     //Maximum output of the PID-controller (+/-)
+float pid_p_gain_yaw = 4.0;                //Gain setting for the pitch P-controller. def 4.0
+float pid_i_gain_yaw = 0.02;               //Gain setting for the pitch I-controller. def 0.02
+float pid_d_gain_yaw = 0.0;                //Gain setting for the pitch D-controller. def 0.0
+int pid_max_yaw = 300;                     //Maximum output of the PID-controller (+/-) def 400
 
 boolean auto_level = true;                 //Auto level on (true) or off (false)
 
