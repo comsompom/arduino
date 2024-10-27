0884df4837f5945f48596526cc3716be6887f999
comsompom <gelfrad@yandex.com>
2024-10-27 10:45:23 +0200

    modify self fly algo

--- self_flyght_stage/self_flyght_stage.ino
+++ self_flyght_stage/self_flyght_stage.ino
@@ -12,6 +12,8 @@ void setup() {
 void loop() {
   check_stage();
   Serial.print("Up - Down: ");
+  Serial.print(stage_fly);
+  Serial.print("  ");
   Serial.println(up_down_signal);
 
   // Serial.print("Timer: ");
@@ -20,67 +22,22 @@ void loop() {
 
 void check_stage() {
   time_counter += 1;
-  if(time_counter == 2000) {
-    if(stage_fly == 0){
-      stage_fly = 1;
-      time_counter = 0;
-    }
+
+  if(time_counter == 2000 & stage_fly <= 6){
+    stage_fly += 1;
+    time_counter = 0;
   }
 
-  if(time_counter == 2000) {
-    if(stage_fly == 1){
-      stage_fly = 2;
-      time_counter = 0;
-    }
-  }
-
-  if(time_counter == 2000) {
-    if(stage_fly == 2){
-      stage_fly = 3;
-      time_counter = 0;
-    }
-  }
-
-  if(time_counter == 2000) {
-    if(stage_fly == 3){
-      stage_fly = 4;
-      time_counter = 0;
-    }
-  }
-
-  if(time_counter == 2000) {
-    if(stage_fly == 4){
-      stage_fly = 5;
-      time_counter = 0;
-    }
-  }
-
-  if(time_counter == 2000) {
-    if(stage_fly == 5){
-      stage_fly = 6;
-      time_counter = 0;
-    }
-  }
-
-  if(time_counter == 2000) {
-    if(stage_fly == 6){
-      stage_fly = 7;
-      time_counter = 0;
-    }
-  }
-
-  if(time_counter == 2000) {
-    if(stage_fly == 7){
-      stage_fly = 0;
-      time_counter = 0;
-    }
+  if(time_counter == 2000 & stage_fly == 7){
+    stage_fly = 0;
+    time_counter = 0;
   }
 
   if(time_counter < 2000){
-    if(stage_fly < 4){
+    if(stage_fly > 0 & stage_fly < 4){
       up_down_signal += (int)((time_counter / 1800));
     }
-    if(stage_fly > 3){
+    if(stage_fly > 3 & stage_fly < 7){
       up_down_signal -= (int)((time_counter / 1800));
     }
   }
