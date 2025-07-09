b39370fb6dc0025a159095eab91748e03a95ce45
comsompom <gelfrad@yandex.com>
2025-07-09 23:11:20 +0300

    udate test gps

--- gps_test/gps_test.ino
+++ gps_test/gps_test.ino
@@ -1,40 +1,51 @@
 #include <SoftwareSerial.h>
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <TinyGPS++.h>
 
-int Rx_pin = 4;
-int Tx_pin = 3;
-bool newStr = 0;
-char symbol = "";
-LiquidCrystal_I2C lcd(0x27,20,4); 
-String outStr = "";
-byte readByte = 0;
-SoftwareSerial SerialGPS(Rx_pin, Tx_pin);
+// For NEO-6M, typical baud rate is 9600.
+// If using software serial (e.g., not on hardware RX/TX pins 0,1):
+SoftwareSerial gpsSerial(4, 3); // RX, TX (connect GPS TX to Arduino pin 2, GPS RX to Arduino pin 3)
 
-void setup()
-{
-  lcd.init();
-  lcd.backlight();
+TinyGPSPlus gps; // The TinyGPS++ object
+
+void setup() {
+  Serial.begin(9600);    // Serial monitor baud rate
+  gpsSerial.begin(9600); // GPS module baud rate
+  Serial.println("Waiting for GPS data...");
 }
 
-void loop()
-{
-  if (SerialGPS.available() > 0){
-    readByte = SerialGPS.read();
-    //Serial.print((char)readByte);
-    //Serial.print(readByte);
-    if (readByte == 36){
-      //Serial.println(outStr);
-      if (outStr.substring(1,6) == "GPRMC"){
-        lcd.setCursor(2,0);
-        lcd.print(outStr.substring(7, 13));
-        lcd.setCursor(2,1);
-        lcd.print(outStr.substring(25, 31))
-        //Serial.println(outStr.substring(7, 13));
-        //Serial.println(outStr.substring(25, 31));
+void loop() {
+  while (gpsSerial.available() > 0) {
+    if (gps.encode(gpsSerial.read())) {
+      // A complete NMEA sentence has been processed
+      if (gps.location.isValid()) {
+        Serial.print("Latitude : ");
+        Serial.println(gps.location.lat(), 6); // 6 decimal places for precision
+        Serial.print("Longitude: ");
+        Serial.println(gps.location.lng(), 6);
+        Serial.print("Altitude : ");
+        Serial.println(gps.altitude.meters(), 2);
+        Serial.print("Satellites: ");
+        Serial.println(gps.satellites.value());
+        Serial.print("HDOP     : ");
+        Serial.println(gps.hdop.value() / 100.0, 2); // HDOP is often reported as integer * 100
+        Serial.print("Speed    : ");
+        Serial.println(gps.speed.knots());
+        Serial.print("Date     : ");
+        Serial.print(gps.date.day());
+        Serial.print("/");
+        Serial.print(gps.date.month());
+        Serial.print("/");
+        Serial.println(gps.date.year());
+        Serial.print("Time     : ");
+        Serial.print(gps.time.hour());
+        Serial.print(":");
+        Serial.print(gps.time.minute());
+        Serial.print(":");
+        Serial.print(gps.time.second());
+        Serial.println();
+      } else {
+        Serial.println("No GPS fix available.");
       }
-      outStr = "";
     }
-    outStr += (char)readByte;
   }
 }
